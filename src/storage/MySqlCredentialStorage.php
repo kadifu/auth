d315b566a86ed6ed251e85b16571f7c331748813
kadifu <kadifu@yeah.net>
2020-07-03 10:10:11 +0800

    Initial commit

---
+++ src/storage/MySqlCredentialStorage.php
@@ -0,0 +1,24 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: appeon
+ * Date: 2020/7/2
+ * Time: 14:18
+ */
+namespace Auth\Storage;
+
+use Auth\Storage\CredentialStorage;
+
+class MySqlCredentialStorage implements CredentialStorage
+{
+    /**
+     * @param string $appId
+     * @return string
+     */
+    public function getPasswordByAppId(string $appId): string
+    {
+        // TODO: Implement getPasswordByAppId() method.
+        return 'password';
+    }
+
+}
